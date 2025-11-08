bd1cac7fe26beeb0d8c8ca0b733e83296bb6d54a
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-11-08 15:24:57 +0530

    added actual cities and countries

--- app/api/admin/fetch-users/route.ts
+++ app/api/admin/fetch-users/route.ts
@@ -12,5 +12,6 @@ export async function GET() {
         }
     } catch (error) {
         console.log(error);
+        return NextResponse.json({ error: "Something went wrong" }, { status: 400 });
     }
 }
