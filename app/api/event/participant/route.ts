92d47f86d8000bd070cc9f3e9af3153296d0de23
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-11-10 18:01:29 +0530

    developed the api route for fetching a particular event participated by the user

---
+++ app/api/event/participant/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userId } = await req.json();
+
+    if (!userId) {
+      return NextResponse.json({ error: "Missing userId" }, { status: 400 });
+    }
+
+    const participant = await prisma.eventParticipant.findMany({
+      where: { userId },
+      include: {
+        event: {
+            include: {
+                cafe: {
+                    select: {
+                        id: true,
+                        name: true,
+                        location: {
+                            select: {
+                                city: true,
+                                country: true
+                            }
+                        },
+                        address: true,
+                        imageUrl: true
+                    }
+                }
+            }
+        }
+      },
+    });
+
+    return NextResponse.json({ message: "Events Found", participant }, { status: 200 });
+  } catch (error) {
+    console.error("Event fetch error:", error);
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}
---
+++ app/queries/participants.ts
@@ -0,0 +1,30 @@
+'use client';
+
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+
+type ApiError = { error: string };
+
+const fetchEventParticipant = async (userId: string) => {
+  try {
+    const res = await axios.post("/api/event/participant", { userId });
+    return res.data.participant;
+  } catch (error) {
+    const axiosErr = error as AxiosError<ApiError>;
+    throw new Error(axiosErr.response?.data?.error || "Something went wrong");
+  }
+};
+
+export function useEventParticipant(userId?: string) {
+  return useQuery({
+    queryKey: ["event-participant", userId],
+    queryFn: () => {
+      if (!userId) throw new Error("User ID is required");
+      return fetchEventParticipant(userId);
+    },
+    enabled: !!userId,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    retry: false,
+  });
+}
