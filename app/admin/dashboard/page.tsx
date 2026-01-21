fa88fd9eb2c6d5781f2ed200ceddd973375896cb
Raza Ali Rahman <aryanrahman90277@gmail.com>
2026-01-21 16:35:48 +0530

    leads and suggestions data added in the dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -5,12 +5,14 @@ import { FaHouseUser } from "react-icons/fa";
 import { MdEvent } from "react-icons/md";
 import { FaMapLocationDot } from "react-icons/fa6";
 import { IoIosCafe } from "react-icons/io";
+import { SiGoogleads } from "react-icons/si";
+import { TbBulbFilled } from "react-icons/tb";
 
 import { useFetchAllAdminData } from '@/app/queries/admin/dashboard';
 import Loader from '@/components/ui/loader';
 
 const Page = () => {
-  const { users, cafes, locations, events, isLoading } = useFetchAllAdminData();
+  const { users, cafes, locations, events, leads, suggestions, isLoading } = useFetchAllAdminData();
 
   if(isLoading) return <Loader />
 
@@ -21,7 +23,7 @@ const Page = () => {
           <h1 className="text-3xl md:text-4xl lg:text-5xl text-[#2f1107] font-semibold text-left mb-4">
             Dashboard
           </h1>
-          <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-10">
+          <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
             <div className='bg-white shadow rounded-xl p-3 flex items-center justify-between gap-3'>
               <div className='w-16 h-16 rounded-full bg-[#ffd100] flex items-center justify-center p-2 text-[#2f1107]'>
                 <FaHouseUser size={30} />
@@ -56,6 +58,24 @@ const Page = () => {
               <div className="flex flex-col items-end gap-1">
                 <span className="text-[#2f1107] text-3xl font-bold">{cafes?.length > 9 ? `${cafes?.length}` : `0${cafes?.length}`}</span>
                 <span className="text-[#202124] font-medium text-base">Cafes</span>
+              </div>
+            </div>
+            <div className='bg-white shadow rounded-xl p-3 flex items-center justify-between gap-3'>
+              <div className='w-16 h-16 rounded-full bg-[#ffd100] flex items-center justify-center p-2 text-[#2f1107]'>
+                <SiGoogleads size={30} />
+              </div>
+              <div className="flex flex-col items-end gap-1">
+                <span className="text-[#2f1107] text-3xl font-bold">{leads?.length > 9 ? `${leads?.length}` : `0${leads?.length}`}</span>
+                <span className="text-[#202124] font-medium text-base">Leads</span>
+              </div>
+            </div>
+            <div className='bg-white shadow rounded-xl p-3 flex items-center justify-between gap-3'>
+              <div className='w-16 h-16 rounded-full bg-[#ffd100] flex items-center justify-center p-2 text-[#2f1107]'>
+                <TbBulbFilled size={30} />
+              </div>
+              <div className="flex flex-col items-end gap-1">
+                <span className="text-[#2f1107] text-3xl font-bold">{suggestions?.length > 9 ? `${suggestions?.length}` : `0${suggestions?.length}`}</span>
+                <span className="text-[#202124] font-medium text-base">Suggestions</span>
               </div>
             </div>
           </div>
--- app/queries/admin/dashboard.ts
+++ app/queries/admin/dashboard.ts
@@ -3,6 +3,8 @@ import { useFetchAllUsers } from '@/app/queries/admin/fetch-users';
 import { useFetchAllCafes } from '@/app/queries/fetch-cafes';
 import { useFetchAllLocations } from '@/app/queries/fetch-locations';
 import { useFetchEvents } from '@/app/queries/get-events';
+import { useFetchUserLeads } from './user-leads';
+import { useFetchSuggestions } from './fetch-suggestions';
 
 export function useFetchAllAdminData() {
   const results = useQueries({
@@ -23,34 +25,50 @@ export function useFetchAllAdminData() {
         queryKey: ['all-events'],
         queryFn: useFetchEvents().queryFn,
       },
+      {
+        queryKey: ['user-leads'],
+        queryFn: useFetchUserLeads().queryFn,
+      },
+      {
+        queryKey: ['suggestions'],
+        queryFn: useFetchSuggestions().queryFn,
+      },
     ],
   });
 
-  const [usersQuery, cafesQuery, locationsQuery, eventsQuery] = results;
+  const [usersQuery, cafesQuery, locationsQuery, eventsQuery, leadsQuery, suggestionsQuery] = results;
 
   const isLoading =
     usersQuery.isLoading ||
     cafesQuery.isLoading ||
     locationsQuery.isLoading ||
-    eventsQuery.isLoading;
+    eventsQuery.isLoading ||
+    leadsQuery.isLoading ||
+    suggestionsQuery.isLoading;
 
   const isError =
     usersQuery.isError ||
     cafesQuery.isError ||
     locationsQuery.isError ||
-    eventsQuery.isError;
+    eventsQuery.isError ||
+    leadsQuery.isError ||
+    suggestionsQuery.isError;
 
   const error =
     usersQuery.error ||
     cafesQuery.error ||
     locationsQuery.error ||
-    eventsQuery.error;
+    eventsQuery.error ||
+    leadsQuery.error ||
+    suggestionsQuery.error;
 
   return {
     users: usersQuery.data,
     cafes: cafesQuery.data,
     locations: locationsQuery.data,
     events: eventsQuery.data,
+    leads: leadsQuery.data,
+    suggestions: suggestionsQuery.data,
     isLoading,
     isError,
     error,
